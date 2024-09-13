e0a84792c338b070f37de3230a97023f6ea51945
Jacek Kosinski <kashiash@gmail.com>
2024-09-13 12:26:02 +0200

    Initial Commit

---
+++ SwiftDataDemo/SwiftDataDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftDataDemoApp.swift
+//  SwiftDataDemo
+//
+//  Created by Jacek Kosinski U on 13/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftDataDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SwiftDataDemoTests/SwiftDataDemoTests.swift
@@ -0,0 +1,16 @@
+//
+//  SwiftDataDemoTests.swift
+//  SwiftDataDemoTests
+//
+//  Created by Jacek Kosinski U on 13/09/2024.
+//
+
+import Testing
+
+struct SwiftDataDemoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
